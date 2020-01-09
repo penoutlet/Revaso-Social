4c463344960e1d589e7b87dedebc73caf55250dd
Sam <samuelriley1393@gmail.com>
2020-01-08 22:07:28 -0500

    initial

---
+++ src/main/java/com/revaso/revaso/RevasoApplication.java
@@ -0,0 +1,13 @@
+package com.revaso.revaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevasoApplication.class, args);
+	}
+
+}
